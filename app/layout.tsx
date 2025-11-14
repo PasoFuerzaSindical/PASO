ce3adb63fe95d42bae7ace7101a527421eb2144f
PasoFuerzaSindical <ugtdandoelpaso@gmail.com>
2025-11-14 21:40:26 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "PASO",
+  description: "Sistema paso",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
